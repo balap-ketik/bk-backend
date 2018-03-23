f4fcfa830bad905a71636c2abbdd3f111befc165
haripermadi <harinurpermadi@gmail.com>
2018-03-23 10:16:35 +0700

    edit leaderboard addscore

--- models/leaderBoard.js
+++ models/leaderBoard.js
@@ -3,15 +3,9 @@ const Schema = mongoose.Schema
 
 const leaderBoardSchema = new Schema ({
   score: Number,
-  // userId: {
-  //   type: Schema.Types.ObjectId,
-  //   ref: 'User'
-  // },
-  username: String,
-  createdAt: {
-    type: Date,
-    default: Date.now
-  }
+  username: String
+},{
+  timestamps: true
 })
 
 module.exports = mongoose.model('LeaderBoard',leaderBoardSchema)
